e26055bb8add14952bfe8d06776cdefd53d7efa2
fieldhawker <fieldhawker+github@gmail.com>
2018-06-24 00:25:06 +0900

    データモデルを追加

---
+++ app/app/src/main/kotlin/com/product/hawker/qiitaappkotlin/model/Article.kt
@@ -0,0 +1,6 @@
+package com.product.hawker.qiitaappkotlin.model
+
+data class Article(val id: String,
+                   val title:String,
+                   val url: String,
+                   val user: User)
---
+++ app/app/src/main/kotlin/com/product/hawker/qiitaappkotlin/model/User.kt
@@ -0,0 +1,5 @@
+package com.product.hawker.qiitaappkotlin.model
+
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)
