48dfa5c1da24dc86b1a0bc0acebdd591ae675ffa
fieldhawker <fieldhawker+github@gmail.com>
2018-06-24 17:51:06 +0900

    API問い合わせ時にプログレスバーを表示する処理を追加

--- app/app/src/main/java/com/product/hawker/qiitaappkotlin/MainActivity.kt
+++ app/app/src/main/java/com/product/hawker/qiitaappkotlin/MainActivity.kt
@@ -1,28 +1,54 @@
 package com.product.hawker.qiitaappkotlin
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.ProgressBar
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
 import com.product.hawker.qiitaappkotlin.client.ArticleClient
 import com.product.hawker.qiitaappkotlin.model.User
 import com.product.hawker.qiitaappkotlin.model.Article
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle.kotlin.bindToLifecycle
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
-
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // 一覧表示用リスト
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+
+        // プログレスバー
+        val progressBar = findViewById(R.id.progress_bar) as ProgressBar
+
+        // 検索ボタン
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button) as Button
+
+        // 記事一覧用アダプタ
+        val listAdapter = ArticleListAdapter(applicationContext)
+
+//        // ダミーデータ投入
+//        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "たろう"),
+//                dummyArticle("Java入門", "じろう"))
+
+        // 一覧の要素をクリックした際の挙動
+        listView.adapter = listAdapter
+        listView.setOnItemClickListener { adapterView, view, position, id ->
+            val intent = ArticleActivity.intent(this, listAdapter.articles[position])
+            startActivity(intent)
+        }
 
         // GSON生成
         val gson = GsonBuilder()
@@ -37,26 +63,22 @@ class MainActivity : AppCompatActivity() {
         // 記事クライアント生成
         val articleClient = retrofit.create(ArticleClient::class.java)
 
-
-        val listAdapter = ArticleListAdapter(applicationContext)
-        listAdapter.articles = listOf(dummyArticle("Kotlin入門", "たろう"),
-                dummyArticle("Java入門", "じろう"))
-
-        val listView: ListView = findViewById(R.id.list_view) as ListView
-        listView.adapter = listAdapter
-        listView.setOnItemClickListener { adapterView, view, position, id ->
-            val article = listAdapter.articles[position]
-            ArticleActivity.intent(this, article).let { startActivity(it)}
-        }
-
         // 検索ボタン
-        val queryEditText = findViewById(R.id.query_edit_text) as EditText
-        val searchButton = findViewById(R.id.search_button) as Button
-
         searchButton.setOnClickListener{
+
+            // クリック時にまずはプログレスバーを表示
+            progressBar.visibility = View.VISIBLE
+
             articleClient.search(queryEditText.text.toString())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .doAfterTerminate{
+
+                        // 終了時にプログレスバーを非表示に
+                        progressBar.visibility = View.GONE
+
+                    }
+                    .bindToLifecycle(this)
                     .subscribe({
                         queryEditText.text.clear()
                         listAdapter.articles = it
